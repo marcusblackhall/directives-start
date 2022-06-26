c2df8ead871290312d2c666b876e4dc87552eab3
Marcus Blackhall <marcusinamsterdam@gmail.com>
2022-06-26 16:07:12 +0200

    Use hostbing to change property

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,27 +1,33 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  constructor(private elRef:ElementRef,private renderer: Renderer2) { }
+  @HostBinding('style.backgroundColor')
+  backgroundColor: string = 'transparent';
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
   ngOnInit(): void {
 
     console.log(this.elRef);
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','azure');   
+  //  this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'azure');
+  this.backgroundColor = 'azure';
 
 
   }
 
   @HostListener("mouseover")
-  mouseOver(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
-  } 
+  mouseOver(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
+  }
 
   @HostListener("mouseleave")
-  mouseOut(){
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
-  } 
+  mouseOut() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
+  }
 
 }
